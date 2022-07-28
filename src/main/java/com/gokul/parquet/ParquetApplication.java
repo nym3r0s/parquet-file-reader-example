e1cc57741fcf64840091caec45612b0d63c9cece
Gokul Srinivas <gokul.srinivas@glovoapp.com>
2022-07-28 11:38:04 +0200

    Example init

---
+++ src/main/java/com/gokul/parquet/ParquetApplication.java
@@ -0,0 +1,31 @@
+package com.gokul.parquet;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.hadoop.fs.Path;
+import org.apache.parquet.avro.AvroParquetReader;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParquetApplication {
+
+  public static void main(String[] args) throws URISyntaxException, IOException {
+    SpringApplication.run(ParquetApplication.class, args);
+    readParquetAndPrint();
+  }
+
+
+  private static void readParquetAndPrint() throws URISyntaxException, IOException {
+    File file =new File(ParquetApplication.class.getClassLoader().getResource("part-00000-42da1110-5dde-4a06-92d2-8cc3805e9acc-c000.snappy.parquet").toURI());
+    Path path = new Path(file.toURI());
+
+    AvroParquetReader<GenericRecord> reader = new AvroParquetReader<GenericRecord>(path);
+    GenericRecord record;
+    while ((record = reader.read()) != null) {
+      System.out.println(record.toString());
+    }
+  }
+}
---
+++ src/test/java/com/gokul/parquet/ParquetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokul.parquet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParquetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
